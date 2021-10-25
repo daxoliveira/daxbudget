c096d6eeea26756915af29294803a6f946f3284e
Dax Oliveira <dax.oliveira.br@gmail.com>
2021-10-25 15:12:31 -0300

    add global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,51 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+    :root {
+        --background: #F0F2F5;
+        --red: #e52e4d;
+        --blue: #5429CC;
+
+        --blue-light: #6933ff;
+        
+        --text-title: #363f5f;
+        --text-body: #969cb3;
+
+        --shape: #FFFFFF;
+    }
+    
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    // REM: 1rem = 16px
+    // Default html 100% = 16px
+
+    // font-size: 16px (desktop)
+    html {
+        @media (max-width: 1080) {
+            font-size: 93.75%; // 15px
+        }
+
+        @media (max-width: 720px) {
+            font-size: 87.5%; // 14px
+        }
+    }
+
+
+    body {
+        background: var(--background);
+        -webkit-font-smoothing: antialiased;
+    }
+
+    button {
+        cursor: pointer;
+    }
+
+    [disabled] {
+        opacity: 0.6;
+        cursor: not-allowed;
+    }
+`
