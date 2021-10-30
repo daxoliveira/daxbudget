094413ff12e12a3efe36bfd5e2cd3d2f3cfa228f
Dax Oliveira <dax.oliveira.br@gmail.com>
2021-10-30 09:17:22 -0300

    create summary component

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import depositsImg from '../../assets/deposits.svg'
+import withdrawalsImg from '../../assets/withdrawals.svg'
+import balanceImg from '../../assets/balance.svg'
+
+import { Container } from "./styles"
+
+export function Summary() {
+    return (
+        <Container>
+            <div>
+                <header>
+                    <p>Deposits</p>
+                    <img src={depositsImg} alt="Deposits" />
+                </header>
+                <strong>$1000.00</strong>
+            </div>
+            <div>
+                <header>
+                    <p>Withdrawals</p>
+                    <img src={withdrawalsImg} alt="Withdrawals" />
+                </header>
+                <strong>(-)$5000.00</strong>
+            </div>
+            <div className="highlight-background">
+                <header>
+                    <p>Balance</p>
+                    <img src={balanceImg} alt="Balance" />
+                </header>
+                <strong>(-)$4000.00</strong>
+            </div>
+        </Container>
+    )
+}
