7a6221854e7e050b79d851ece2abe4fab13ffac1
Dax Oliveira <dax.oliveira.br@gmail.com>
2022-01-18 16:22:24 -0300

    migrate transaction creation to context api

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -9,11 +9,20 @@ interface Transaction {
     createdAt: string;
 }
 
-export const TransactionsContext = createContext<Transaction[]>([]);
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
 
 interface TransactionsProviderProps {
     children: ReactNode;
 }
+
+interface TransactionsContextData {
+    transactions: Transaction[];
+    createTransaction: (transaction: TransactionInput) => void;
+}
+
+export const TransactionsContext = createContext<TransactionsContextData>(
+    {} as TransactionsContextData
+);
 
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
     const [transactions, setTransactions] = useState<Transaction[]>([]);
@@ -23,8 +32,12 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
             .then(response => setTransactions(response.data.transactions))
     }, []);
 
+    function createTransaction(transaction: TransactionInput) {
+        api.post('/transactions', transaction)
+    }
+
     return (
-        <TransactionsContext.Provider value={transactions}>
+        <TransactionsContext.Provider value={{ transactions, createTransaction }}>
             {children}
         </TransactionsContext.Provider>
     )
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,12 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useState, useContext } from 'react';
 import Modal from 'react-modal';
+import { TransactionsContext } from '../../TransactionsContext';
+import { api } from '../../services/api';
+
 import closeImg from '../../assets/close.svg';
 import despositsImg from '../../assets/deposits.svg';
 import withdrawalsImg from '../../assets/withdrawals.svg'
-import { api } from '../../services/api';
+
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
 
 interface NewTransactionModalProps {
@@ -12,22 +15,22 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+    const { createTransaction } = useContext(TransactionsContext);
+
     const [title, setTitle] = useState('');
-    const [value, setValue] = useState(0);
+    const [amount, setAmount] = useState(0);
     const [category, setCategory] = useState('');
-    const [radioBoxType, setRadioBoxType] = useState('deposit');
+    const [type, setType] = useState('deposit');
 
     function handleCreateNewTransaction(event: FormEvent) {
         event.preventDefault();
 
-        const data = {
+        createTransaction({
             title,
-            value,
+            amount,
             category,
-            radioBoxType
-        };
-
-        api.post('/transactions', data)
+            type
+        })
     };
 
     return (
@@ -57,15 +60,15 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                 <input
                     type="number"
                     placeholder="Amount"
-                    value={value}
-                    onChange={event => setValue(Number(event.target.value))}
+                    value={amount}
+                    onChange={event => setAmount(Number(event.target.value))}
                 />
 
                 <TransactionTypeContainer>
                     <RadioBox
                         type="button"
-                        onClick={() => setRadioBoxType('deposit')}
-                        isActive={radioBoxType === 'deposit'}
+                        onClick={() => setType('deposit')}
+                        isActive={type === 'deposit'}
                         activeColor="green"
                     >
                         <img src={despositsImg} alt="Deposit" />
@@ -73,8 +76,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                     </RadioBox>
                     <RadioBox
                         type="button"
-                        onClick={() => setRadioBoxType('withdraw')}
-                        isActive={radioBoxType === 'withdraw'}
+                        onClick={() => setType('withdraw')}
+                        isActive={type === 'withdraw'}
                         activeColor="red"
                     >
                         <img src={withdrawalsImg} alt="Withdrawal" />
